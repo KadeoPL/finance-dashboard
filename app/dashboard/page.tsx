ebbb6bb82d17894a468a285801518af5de63aa30
KadeoPL <stopkakrzysztof1@gmail.com>
2026-02-16 22:04:37 +0100

    add search input

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { Sidebar } from "@/components/ui/sidebar";
 import { SidebarTrigger, SidebarProvider } from "@/components/ui/sidebar";
 import TransactionsList from "@/features/transactions/components/TransactionsList";
+import { Input } from "@/components/ui/input";
 
 export default function page() {
   return (
     <SidebarProvider>
       <Sidebar />
-      <div className="w-full h-full">
-        <div className="w-full h-10 bg-red-500">
-          <div>
+      <div className="w-full h-screen">
+        <div className="w-full h-screen flex flex-col">
+          <div className="w-full h-16 flex items-center justify-between pr-12 pl-4">
             <SidebarTrigger />
+            <Input placeholder="Type text" className="max-w-2/3 border-b-2" />
           </div>
-          <div>
+
+          <div className="flex-1 overflow-auto bg-secondary p-12">
             <TransactionsList />
           </div>
         </div>
